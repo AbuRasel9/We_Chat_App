72bf47b987b1866f76ba7bfb741051ac0e2d11af
AbuRasel9 <aburaselahamed99@gmail.com>
2023-07-02 00:03:53 +0600

    home screen done

---
+++ lib/Screens/home_screens.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //Home button add in
+        leading:  Icon(Icons.home,),
+        title: const Text("We Chat"),
+        actions: [
+          IconButton(
+              disabledColor: Colors.greenAccent,
+              onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+      //floating acction button add for add user
+      floatingActionButton: Padding(
+        padding:const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+          child: const Icon((Icons.add_comment_rounded)),
+          onPressed: (){},
+        ),
+      ),
+    );
+  }
+}
